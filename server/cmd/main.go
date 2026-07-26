628c356ef957f1e0455bd8ecf6a4d2b25611ee3d
abde1khaliq <abdelkhaliqqq@gmail.com>
2026-07-26 21:56:40 +0300

    remove agent folder for now.

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,1 +1,28 @@
-package cmd
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		resp, err := http.Get("http://localhost:11434/api/tags")
+		if err != nil {
+    		log.Panic(err)
+		}
+		defer resp.Body.Close()
+		body, _ := io.ReadAll(resp.Body)
+
+		c.String(http.StatusOK, string(body))
+	})
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.Run(":8080")
+}
